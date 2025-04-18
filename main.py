d9d19845785e41c8f54d06d0f5ef0bb07f1048cc
Peter <Peterinsinga83@gmail.com>
2025-04-17 21:50:19 -0400

    created player object

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
+Clock = pygame.time.Clock()
+dt = 0
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-def game_loop():
+def main():
+    pygame.init()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    game_loop(player)
+
+
+
+
+
+
+def game_loop(player):
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        Clock.tick(60)
         
-
-def main():
-    pygame.init()
-    game_loop()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        
 
 
 
